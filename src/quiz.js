f73d004131fd9cf6c6c4febfbe193acf80641720
RFelix1790 <rmsfelix19@gmail.com>
2025-05-06 18:14:37 +0200

    done

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,43 @@
 class Quiz {
     // YOUR CODE HERE:
     //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+     constructor (questions, timeLimit, timeRemaining){
+        this.questions = questions
+        this.timeLimit = timeLimit
+        this.timeRemaining = timeRemaining
+        this.correctAnswers = 0
+        this.currentQuestionIndex = 0
+     }
 
-    // 2. getQuestion()
+     getQuestion(){
+        return this.questions[this.currentQuestionIndex]
+     }
     
-    // 3. moveToNextQuestion()
+     moveToNextQuestion(){
+        this.currentQuestionIndex +=1
+     }
 
-    // 4. shuffleQuestions()
+     shuffleQuestions(){
+        this.questions.sort(() => Math.random() - 0.5)
+     }
 
-    // 5. checkAnswer(answer)
+     checkAnswer(answer){
+        if (this.answer === this.questions[this.currentQuestionIndex].answer){
 
-    // 6. hasEnded()
+        }
+        this.correctAnswers +=1
+        }
+        
+
+     hasEnded() {
+        if (this.currentQuestionIndex < this.questions.length) {
+            return false
+        }
+        else if (this.currentQuestionIndex === this.questions.length){
+            return true
+        }
+    }
+    filterQuestionsByDifficulty(difficulty){
+      
+    }
 }
